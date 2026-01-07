e19e06868353f2c985325574e3ddc0d3bd68c6f7
Chetna Ambe <chetnaambe75@gmail.com>
2026-01-08 00:55:15 +0530

    SPOTLIGHT EFFECT

--- Spotlight effect/script.js
+++ Spotlight effect/script.js
@@ -1,4 +1,19 @@
 addEventListener("mousemove",(e)=>{
     document.body.style.setProperty(`--x`,e.clientX+`px`);
     document.body.style.setProperty(`--y`,e.clientY +`px`);
+
+
+
+
+
 })
+// const btn = document.querySelector("#sound");
+const vid = document.querySelector("#bg");
+vid.addEventListener("click",()=>{
+     vid.muted = false;
+    vid.volume = 1;
+    vid.play();
+
+});
+
+
